c3ce32e5db6e051353a9ef23337bbf1b5b274550
Thijs Leegwater <tleegwater@gmail.com>
2014-10-28 08:47:39 +0100

    testing

--- bravoctl.pl
+++ bravoctl.pl
@@ -7,17 +7,13 @@ use IPC::Run qw( start );
 
 my $bravo2 = Bravo2->new();
 
-for (my $i=1; $i <= 1; $i++) {
-
-
-
 	$bravo2->resetDevice;
 	#sleep 15;
 	#$bravo2->sendCommand('0x05');
 	
 	#my $driveno = `drutil list|grep USB|cut -d" " -f1`;
 	my $driveno = `lsblk|grep rom|cut -d" " -f1`;
-
+	chomp($driveno);
 
 	print "Drive: ".$driveno."\n";
 	
@@ -28,44 +24,46 @@ for (my $i=1; $i <= 1; $i++) {
 	#$bravo2->sendCommand('0x80'); #get left put in drive (stays busy) 
 	#$bravo2->sendCommand('0x81'); #get left, put in printer
 	#$bravo2->sendCommand('0x82'); #get left, put right
-	$bravo2->sendCommand('0x83'); #get right, put in drive
 	
 
-	system("drutil", "-drive", $driveno, "tray", "close");
-	system('cdclose', '/dev/'. $driveno);
+	##$bravo2->sendCommand('0x83'); #get right, put in drive
+	
 
-	sleep 30;
-	my $partno = `df -lH|grep "100%"|grep -v "MobileBackups"|cut -d" " -f1|head -1`;
-	chomp($partno);
-	my $devno = `df -lH|grep "100%"|grep -v "MobileBackups"|cut -d" " -f1|sed s"/s[0-9]//"`;
-	chomp($devno);
-	my $command = "diskutil info ".$devno."|grep \"Volume Name\"|cut -d\":\" -f2|sed s\"/\ *//\"";
-	my $volname = `$command`;
-	chomp($volname);
+	##system("drutil", "-drive", $driveno, "tray", "close");
+	##system('cdclose', '/dev/'. $driveno);
+
+	##sleep 30;
+	##my $partno = `df -lH|grep "100%"|grep -v "MobileBackups"|cut -d" " -f1|head -1`;
+	##chomp($partno);
+	##my $devno = `df -lH|grep "100%"|grep -v "MobileBackups"|cut -d" " -f1|sed s"/s[0-9]//"`;
+	##chomp($devno);
+	##my $command = "diskutil info ".$devno."|grep \"Volume Name\"|cut -d\":\" -f2|sed s\"/\ *//\"";
+	##my $volname = `$command`;
+	##chomp($volname);
 	#my $volname = system("diskutil", "info", $partno, "|", "grep", "\"Volume Name: \"", "|", "sed", "s\"/Volume\ Name\://\"", "|", "sed s\"/\ *//\"");
-	my $now_string = strftime "%Y%m%d_%H%M%S", localtime;
-	print $devno."\n";
-	print $now_string."\n";
+	##my $now_string = strftime "%Y%m%d_%H%M%S", localtime;
+	##print $devno."\n";
+	##print $now_string."\n";
 	
 	#$h = start ['ddrescue', '-n','-b2048',$partno,'/Users/picturae/Desktop/'.$now_string.'.iso','/Users/picturae/Desktop/'.$now_string.'.log'], $out;
-	$h = start ['diskutil', 'unmount', $partno], $out;
-	$h->finish;
+	##$h = start ['diskutil', 'unmount', $partno], $out;
+	##$h->finish;
 	#$h = start ['hdiutil', 'makehybrid', '-ov', '-o', '/Volumes/ZEB_9878_D_001/DVDRIP/'.$now_string.'_'.$volname.'.iso', $devno], $out;
 	#$h->finish;
 
-	$h = start ['dd', 'bs=20480', 'if='.$devno, 'of=/Volumes/ZEB_9878_D_001/DVDRIP/'.$now_string.'_'.$volname.'.iso'], $out;
-	$h->finish;
+	##$h = start ['dd', 'bs=20480', 'if='.$devno, 'of=/Volumes/ZEB_9878_D_001/DVDRIP/'.$now_string.'_'.$volname.'.iso'], $out;
+	##$h->finish;
 
 	#finish $h
 	#$h->kill_kill;
 	
-	system("drutil", "-drive", $driveno, "tray", "eject");
+	##system("drutil", "-drive", $driveno, "tray", "eject");
 	#$bravo2->sendCommand('0x84'); #get right, put in printer
 	#$bravo2->sendCommand('0x85'); #get right, put left
 	#$bravo2->sendCommand('0x86'); #drive to printer
 	#$bravo2->sendCommand('0x87'); #drive to right
-	$bravo2->sendCommand('0x88'); #drive to left
-	system("drutil", "-drive", $driveno, "tray", "close");
+	##$bravo2->sendCommand('0x88'); #drive to left
+	##system("drutil", "-drive", $driveno, "tray", "close");
 	#$bravo2->sendCommand('0x89'); #drive to drive (kick from printer)
 	#$bravo2->sendCommand('0x8a'); #get printer, put right (stays busy)
 	#$bravo2->sendCommand('0x8b'); #get printer, put left
@@ -85,7 +83,7 @@ for (my $i=1; $i <= 1; $i++) {
 	#`drutil -drive 1 tray close`;
 	
 	print "Done\n";
-}
+
 
 
 
